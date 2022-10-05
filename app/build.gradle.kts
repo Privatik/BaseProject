67f789bbe6967aa754369e74ce09bec358ac22ba
Android dev <i.kurak@complitech.org>
2022-10-05 11:05:22 +0300

    setting dependencies for my project and update gitIgnore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlinx-serialization")
 }
 
 apply(from =  rootProject.file("dependencies.gradle.kts"))
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "io.my.baseproject"
         minSdk = 24
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -41,7 +43,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = rootProject.extra.get("composeVersion") as String
+        kotlinCompilerExtensionVersion = rootProject.extra.get("composeCompileVersion") as String
     }
     packagingOptions {
         resources {
@@ -55,8 +57,43 @@ dependencies {
     implementation(rootProject.extra.get("coreKtx") as String)
     implementation(rootProject.extra.get("activityCompose") as String)
 
+    implementation(rootProject.extra.get("accompanistInsets") as String)
+    implementation(rootProject.extra.get("accompanistSystemUiController") as String)
+
+    implementation(rootProject.extra.get("machine") as String)
+
+    implementation(rootProject.extra.get("dagger") as String)
+    kapt(rootProject.extra.get("daggerCompiler") as String)
+
+    implementation(rootProject.extra.get("dataStorePreferences") as String)
+    implementation(rootProject.extra.get("dataStore") as String)
+    implementation(rootProject.extra.get("protobufJavalite") as String)
+    implementation(rootProject.extra.get("preferenceKtx") as String)
+
+    implementation(rootProject.extra.get("kotlinxSerializationJson") as String)
+
+    implementation(rootProject.extra.get("ktorClientCio") as String)
+    implementation(rootProject.extra.get("ktorClientAndroid") as String)
+    implementation(rootProject.extra.get("ktorClientAuth") as String)
+    implementation(rootProject.extra.get("ktorClientSerialization") as String)
+    implementation(rootProject.extra.get("ktorClientCore") as String)
+    implementation(rootProject.extra.get("ktorClientWebsocket") as String)
+    implementation(rootProject.extra.get("ktorClientLoggingJvm") as String)
+
     (rootProject.extra.get("compose") as List<*>).forEach { compose ->
         implementation(compose as String)
     }
+    (rootProject.extra.get("coil") as List<*>).forEach { compose ->
+        implementation(compose as String)
+    }
+
+    (rootProject.extra.get("sqlDelight") as List<*>).forEach { compose ->
+        implementation(compose as String)
+    }
+
+    (rootProject.extra.get("presenter") as List<*>).forEach { compose ->
+        implementation(compose as String)
+    }
+
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
+buildscript {
+    dependencies {
+        classpath("com.squareup.sqldelight:gradle-plugin:1.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.21")
+    }
+}
+
+
 plugins {
     id("com.android.application") version "7.2.2" apply false
     id("com.android.library") version "7.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
 }
 
-apply(from =  rootProject.file("dependencies.gradle.kts"))
+apply(from = rootProject.file("dependencies.gradle.kts"))
 
 tasks {
     register("clean", Delete::class) {
